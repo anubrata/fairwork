af0a513e5bb1573f789a825438cb8c7e47dbd502
Anubrata Das <2480260+anubrata@users.noreply.github.com>
2020-09-12 12:15:00 -0500

    temporary commit, deplyment not working

--- fairwork_server/local_settings.py
+++ fairwork_server/local_settings.py
@@ -6,7 +6,7 @@ DEBUG = True
 TIME_ZONE = 'America/Los_Angeles' # or your timezone
 MINIMUM_WAGE_PER_HOUR = 10
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ["0.0.0.0", "127.0.0.1", ".herokuapp.com"]
 
 EMAIL_HOST = '' # email server so you can send notifications from the platform, e.g., SendGrid
 EMAIL_HOST_USER = '' # username
@@ -21,4 +21,7 @@ ADMINS = [(ADMIN_NAME, ADMIN_EMAIL), ]
 WORKER_IRB_TEMPLATE = 'placeholder-irb-worker.html' # for your IRB agreement
 REQUESTER_IRB_TEMPLATE = 'placeholder-irb-requester.html' # for your IRB agreement
 
-HOSTNAME = 'https://fairwork-utircs.herokuapp.com' # used as the hostname in emails sent by the system, since Django management commands do not know the server's hostname
+HOSTNAME = 'https://fairwork-ut.herokuapp.com' # used as the hostname in emails sent by the system, since Django management commands do not know the server's hostname
+
+# Activate Django-Heroku.
+django_heroku.settings(locals())
